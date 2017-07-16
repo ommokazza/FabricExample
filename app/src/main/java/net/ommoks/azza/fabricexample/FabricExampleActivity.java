b8d96ff972a57189d672db12699caf62b51705f6
ommokazza <ommokazza@gmail.com>
2017-07-16 13:27:37 +0900

    Apply Crashlystics kit

--- app/src/main/java/net/ommoks/azza/fabricexample/FabricExampleActivity.java
+++ app/src/main/java/net/ommoks/azza/fabricexample/FabricExampleActivity.java
@@ -2,12 +2,22 @@ package net.ommoks.azza.fabricexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 
 public class FabricExampleActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_fabric_example);
+    }
+
+    public void forceCrash(View view) {
+        throw new RuntimeException("This is a crash");
     }
 }
